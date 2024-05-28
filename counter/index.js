f56417c4708fdbd427fd322ba4f6cde89018a2f7
ali <mhmmtkilicc@gmail.com>
2024-05-28 13:55:13 +0300

    css configuration

--- counter/index.js
+++ counter/index.js
@@ -1,15 +1,15 @@
 let num=0;
 document.getElementById("increase").onclick=function() {
     num++;
-    document.getElementById("myp").textContent=num;
+    document.getElementById("countArea").textContent=num;
 }
 document.getElementById("reset").onclick=function() {
     num=0;;
-    document.getElementById("myp").textContent=num;
+    document.getElementById("countArea").textContent=num;
 
 }
 document.getElementById("decrease").onclick=function() {
     num--;
-    document.getElementById("myp").textContent=num;
+    document.getElementById("countArea").textContent=num;
 
 }
