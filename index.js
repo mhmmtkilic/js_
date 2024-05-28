b8c1a8e8e741fd499f13e3c5c24290e1add4c9d3
ali <mhmmtkilicc@gmail.com>
2024-05-28 12:29:00 +0300

    new update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("I like pizza");
 window.alert("this is a alert message");
 window.alert("i like pizza"); 
 
-*/
+
 let age = 22;
 let FirstName = "Ali";
 document.getElementById("p1").textContent =`My name is ${FirstName}`;
@@ -14,3 +14,21 @@ document.getElementById("mySubmit").onclick = function(){
 username = document.getElementById("myText").value;
 console.log(username);
 }
+
+let age;
+document.getElementById("mySubmit").onclick = function(){
+    age=document.getElementById("myText").value;
+    console.log(age);
+}
+
+// type conversions 
+let x;
+let y;
+let z;
+x= Number(x);
+y=String(y);
+z=Boolean(z);
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
+*/
